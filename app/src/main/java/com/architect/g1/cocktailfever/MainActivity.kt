52f2241aae28ee3433a0bec7937cf30e93e4ad73
Raúl Cristóbal liz <me@raulcristobal.com>
2020-03-18 21:16:43 +0100

    Crea proyecto

---
+++ app/src/main/java/com/architect/g1/cocktailfever/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.architect.g1.cocktailfever
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
