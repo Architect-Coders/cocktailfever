c1df93481e4c56f78018248b687b9c7e7b75e329
Raúl Cristóbal liz <me@raulcristobal.com>
2020-05-03 10:35:59 +0200

    Crea CoctelAdapter

---
+++ app/src/main/java/com/architect/g1/cocktailfever/ui/main/CoctelAdapter.kt
@@ -0,0 +1,40 @@
+package com.architect.g1.cocktailfever.ui.main
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.architect.g1.cocktailfever.R
+import com.architect.g1.cocktailfever.domain.Coctel
+import com.architect.g1.cocktailfever.ui.common.inflate
+import com.architect.g1.cocktailfever.ui.common.loadUrl
+import kotlinx.android.synthetic.main.view_coctel_item.view.*
+
+private typealias CoctelListener = (Coctel) -> Unit
+
+class CoctelAdapter (
+    private val items:List<Coctel> = emptyList() ,
+    private val listener:CoctelListener) : RecyclerView.Adapter<CoctelAdapter.ViewHolder>(){
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view:View=parent.inflate(R.layout.view_coctel_item)
+        return ViewHolder(view,listener)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount()= items.size
+
+    class ViewHolder(view: View, private val listener: CoctelListener):
+        RecyclerView.ViewHolder(view) {
+
+        fun bind(coctelItem: Coctel){
+            itemView.txtTitle .text = coctelItem.nombre
+            itemView.imgThumb.loadUrl(coctelItem.thumbUrl)
+
+            itemView.setOnClickListener { listener(coctelItem) }
+        }
+    }
+}
