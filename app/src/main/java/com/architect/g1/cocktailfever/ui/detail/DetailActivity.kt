5025ba90f4a1976680d481a518f3835e5b68d761
Raúl Cristóbal liz <me@raulcristobal.com>
2020-05-11 07:46:37 +0200

    Crea DetailActivity

---
+++ app/src/main/java/com/architect/g1/cocktailfever/ui/detail/DetailActivity.kt
@@ -0,0 +1,13 @@
+package com.architect.g1.cocktailfever.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.architect.g1.cocktailfever.R
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
