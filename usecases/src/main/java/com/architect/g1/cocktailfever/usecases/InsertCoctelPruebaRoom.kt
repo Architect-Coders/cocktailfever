db6154b53c4f8e833c444282d3266bed6baf0a0f
Antonio Tobajas <atobajas@gmail.com>
2020-05-14 12:15:18 +0200

    Elimino pruebas de Room en usescases y MainActivity

--- usecases/src/main/java/com/architect/g1/cocktailfever/usecases/InsertCoctelPruebaRoom.kt
+++
@@ -1,8 +0,0 @@
-package com.architect.g1.cocktailfever.usecases
-
-import com.architect.g1.cocktailfever.data.repository.CoctelesRepository
-import com.architect.g1.cocktailfever.domain.Coctel
-
-class InsertCoctelPruebaRoom(private val coctelesRepository: CoctelesRepository) {
-    suspend fun invoke(coctel: Coctel) = coctelesRepository.insertCoctel(coctel)
-}
