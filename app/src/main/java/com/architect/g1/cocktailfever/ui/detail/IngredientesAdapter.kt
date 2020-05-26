15bf755d726025cd86af512883c17e5125e6a23d
Raúl Cristóbal liz <me@raulcristobal.com>
2020-05-27 01:58:05 +0200

    Crea IngredientesAdapter

---
+++ app/src/main/java/com/architect/g1/cocktailfever/ui/detail/IngredientesAdapter.kt
@@ -0,0 +1,38 @@
+package com.architect.g1.cocktailfever.ui.detail
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.architect.g1.cocktailfever.R
+import com.architect.g1.cocktailfever.data.database.entity.Ingrediente
+import com.architect.g1.cocktailfever.ui.common.basicDiffUtil
+import com.architect.g1.cocktailfever.ui.common.inflate
+import kotlinx.android.synthetic.main.view_coctel_ingredientes_item.view.*
+
+class IngredientesAdapter
+    : RecyclerView.Adapter<IngredientesAdapter.ViewHolder>() {
+
+    var items:List<Ingrediente> by basicDiffUtil(
+        emptyList(),
+        areItemsTheSame = { old,new -> old.nombre==new.nombre}
+    )
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view:View=parent.inflate(R.layout.view_coctel_ingredientes_item,false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount()= items.size
+
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        fun bind(ingredienteItem: Ingrediente){
+            //itemView.imagen_ingrediente.loadUrl(Ingrediente.thumbUrl)
+            itemView.nombre_igrediente.text = ingredienteItem.nombre
+            itemView.cantidad_igrediente.text=ingredienteItem.medida
+        }
+    }
+}
